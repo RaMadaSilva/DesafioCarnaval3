c3ac23c9fc67cdeb6415b279f8229b4bb7899bbf
RaMadaSilva <raul01mateia@gmail.com>
2023-02-19 12:59:15 +0100

    initial

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace DesafioCarnaval3;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Informa o Preço do alcool R$: ");
+        double.TryParse(Console.ReadLine(), out double alcool);
+        Console.Write("Informa o Preço da Gasolina R$: ");
+        double.TryParse(Console.ReadLine(), out double gasolina);
+
+        var correlacao = CoefienteEconomino(alcool, gasolina) * 100;
+        var vantagemEconomina = Validar(correlacao);
+
+        Console.WriteLine($"{correlacao.ToString("F2", CultureInfo.InvariantCulture)} %, \n{vantagemEconomina}");
+    }
+
+    private static string Validar(double correlacao)
+    {
+        if (correlacao >= 65)
+            return "Vantagem em abastencer o tanque com alcool";
+        return "vantagem em abasteccer o tanque com gasolina";
+    }
+
+    private static double CoefienteEconomino(double alcool, double gasolina)
+            => alcool / gasolina;
+}
